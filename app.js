3dd81345476dc3256f064459cb4a2ba4ecc1d189
Roquverse <123464792+Roquverse@users.noreply.github.com>
2023-01-24 12:50:11 +0100

    Todo List app

---
+++ app.js
@@ -0,0 +1,10 @@
+let userPurchase = document.getElementById('u1').value;
+let pass = document.getElementById('p2').value;
+
+let username = document.querySelector('.userName');
+let password = document.querySelector('.userPass');
+
+function submitForm() {
+    username.append(user);
+    password.append(pass);
+}
